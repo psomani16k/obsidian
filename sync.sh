1118d7e010ae7d3f1436ee66ab95c782d78611e3
parth_tab <psomani16k@gmail.com>
2025-08-18 01:05:01 +0530

    update on: 01:05 18-08-2025

--- sync.sh
+++ sync.sh
@@ -3,37 +3,29 @@
 dt=$(date +"%H:%M %d-%m-%Y")
 obsidian_root="./"
 
-result=$(
-  {
-    echo "$dt
+if [ -n "$TERMUX_VERSION" ]; then
+  obsidian_root="$HOME/storage/shared/obsidian"
+  cd "$obsidian_root" || { echo "[ERROR] Repo path not found."; exit 1; }
+fi
+
+{
+  echo "
+$dt
 ================
-    "
-    if ping -c 1 -W 3 www.github.com >/dev/null 2>&1; then
-      echo "[OK] Internet connection detected."
-    else
-      echo "[ERROR] No internet connection. Exiting."
-      exit 1
-    fi
+"
+  if ping -c 1 -W 3 www.github.com >/dev/null 2>&1; then
+    echo "[OK] Internet connection detected."
+  else
+    echo "[ERROR] No internet connection. Exiting."
+    exit 1
+  fi
 
-    if [ -n "$TERMUX_VERSION" ]; then
-      echo "Running inside Termux"
-      obsidian_root="$HOME/storage/shared/obsidian"
-      cd "$obsidian_root" || { echo "[ERROR] Repo path not found."; exit 1; }
-    else
-      echo "Running on regular Linux"
-    fi
-
-
-    dt=$(date +"%H:%M %d-%m-%Y")
-
-    git add .
-    git commit -m "update on: $dt" 
-    git pull --rebase || {
-      echo "[WARN] Conflict detected, resolving in favor of local changes..."
-      git checkout --ours -- .
-    }
-    git push
+  git add .
+  git commit -m "update on: $dt" 
+  git pull --rebase || {
+    echo "[WARN] Conflict detected, resolving in favor of local changes..."
+    git checkout --ours -- .
   }
-)
+  git push
+} &>> "$obsidian_root/log"
 
-echo "$result" >> "$obsidian_root/log"
