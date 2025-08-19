e744160c2d802b33985a4a4f7e672aae468542b5
parth_somani <parth.s@even.in>
2025-08-19 14:12:56 +0530

    update on: 14:12 19-08-2025

--- status.sh
+++ status.sh
@@ -1,2 +1,10 @@
+obsidian_root="./"
+
+if [ -n "$TERMUX_VERSION" ]; then
+  obsidian_root="$HOME/storage/shared/obsidian"
+  cd "$obsidian_root" || { echo "[ERROR] Repo path not found."; exit 1; }
+fi
+
 git status
+
 exit 1
